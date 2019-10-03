3467a3fd534e70c2c64c6df720878c9598eedc23
Ramirez <aramirez56@dxc.com>
2019-10-03 13:07:25 +0200

    Subimos proyectos de prueba

---
+++ proyecto2/src/test/java/com/dxc/demo/proyecto2/Proyecto2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dxc.demo.proyecto2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Proyecto2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ proyecto4/src/test/java/com/dxc/demo/proyecto4/Proyecto4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dxc.demo.proyecto4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Proyecto4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
